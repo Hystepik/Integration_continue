78daf4c6a2e1c1843305ff29586accfe9b7dd6e3
Sarnault <sa.arnault@gmail.com>
2021-01-12 10:02:17 +0100

    changement test

--- web_app/src/test/java/AccueilControllerTest.java
+++ web_app/src/test/java/AccueilControllerTest.java
@@ -1,4 +1,8 @@
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,7 +18,7 @@ public class AccueilControllerTest{
 	}
 	@Test
 	void testListProducts() {
-		//assertEquals(false, false);
+		assertTrue(true);
 	}
 
 }
